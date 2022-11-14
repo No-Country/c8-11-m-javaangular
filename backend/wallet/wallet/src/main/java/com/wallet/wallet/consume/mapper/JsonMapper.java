1ac994b4b44eef6b1648bf2a600c6d3b4d3a8d2f
Lucas López <lucaslopezd@hotmail.com>
2022-11-14 19:00:26 -0300

    jsonMapper refactor / annotation added

--- backend/wallet/wallet/src/main/java/com/wallet/wallet/consume/mapper/JsonMapper.java
+++ backend/wallet/wallet/src/main/java/com/wallet/wallet/consume/mapper/JsonMapper.java
@@ -1,9 +1,12 @@
 package com.wallet.wallet.consume.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Component
 public record JsonMapper(ObjectMapper mapper) {
 
     public <S, D> D map(String json, Class<D> destinationClass)

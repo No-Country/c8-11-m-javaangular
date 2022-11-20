2935897f49dfb8de0514c931d3e5923b7cabba1b
Santiago <monitocastellani@gmail.com>
2022-11-20 12:25:35 -0300

    ordenando module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LandingComponent } from './components/landing/landing.component';import { GastosComponent } from './components/gastos/gastos.component';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, /*HTTP_INTERCEPTORS*/ } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Components
+import { LandingComponent } from './components/landing/landing.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 import { CreatorsComponent } from './components/landing/creators/creators.component';
 
+// Modules
 import { AuthModule } from './modules/auth/auth.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
-import { HttpClientModule, /*HTTP_INTERCEPTORS*/ } from '@angular/common/http';
 
 @NgModule({
   declarations: [

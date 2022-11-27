daac642d478a4048d6a686bcfb58fc8d514b98dd
leonelpb <leonelperez.b23@gmail.com>
2022-11-27 18:11:01 -0300

    Iniciar estadisticas 1/2

--- frontend/src/app/modules/dashboard/pages/general/general.component.ts
+++ frontend/src/app/modules/dashboard/pages/general/general.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-general',
@@ -10,6 +12,47 @@ export class GeneralComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.renderChart('myChart')
   }
 
+  renderChart( id:any){
+    const myChart = new Chart (id, {
+      type: "line",
+      data: {
+        labels:['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre'] ,
+        datasets: [{
+          label: 'Gastos',
+          data:[3000,4000,5000,3000,3000,4000,5000,3000,7000,10000] ,
+          borderColor: [
+            'rgba(93, 33, 210)'
+          ],
+          backgroundColor:
+          ['rgba(93, 33, 210, 0.2)'],
+          tension: 0.2,
+          fill:true
+        },
+        {
+          label: 'Ingresos',
+          data:[2000,3000,4000,5000,7000,12000,12000,3500,2000,6000],
+          borderColor: [
+            'rgba(255, 99, 12, 1)'
+          ],
+          backgroundColor:[
+            'rgba(255, 99, 12, 0.2)'
+          ],
+          tension: 0.2,
+          fill:true
+        }]
+      },
+      options:{
+        plugins:{
+          legend:{
+            labels:{
+              
+            }
+          }
+        }
+      }           
+    });
+   }
 }

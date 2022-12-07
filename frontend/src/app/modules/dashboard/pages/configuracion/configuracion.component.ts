45ffd57c20fe2cea06a46bf517d1b11c8dd8c048
Santiago <monitocastellani@gmail.com>
2022-12-07 18:08:43 -0300

    configuracion

--- frontend/src/app/modules/dashboard/pages/configuracion/configuracion.component.ts
+++ frontend/src/app/modules/dashboard/pages/configuracion/configuracion.component.ts
@@ -7,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracionComponent implements OnInit {
 
+  firstName:string="Usuario";
+  lastName:string="Registrado";
+  email:string="usuario@email.com";
+  editar:boolean=true;
+  light:boolean=true;
+
+
   constructor() { }
   
-  ngOnInit(): void {}  
+  ngOnInit(): void {
+    const nombre = sessionStorage.getItem("firstName");
+    const apellido = sessionStorage.getItem("lastName");
+    const email = sessionStorage.getItem("email");
+    if (nombre !==null){
+      this.firstName=nombre
+    }
+    if (apellido !== null) {
+      this.lastName = apellido
+    }
+    if (email !== null){
+      this.email = email
+    }
+  }  
+
+  cambioRegistro(){
+    console.log("Cambiar Registro")
+  }
+  restablecer(){
+    console.log("Restablecer")
+  }
 
 }
